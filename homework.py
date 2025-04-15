3ae65097700a561c31d9d267daeefde8ec6d1f8e
yevgeny_novik <novik297443991@yandex.ru>
2025-04-15 15:02:11 +0300

    Отправка работы на 2-е ревью по 12 спринту

--- homework.py
+++ homework.py
@@ -4,22 +4,17 @@ from logging.handlers import RotatingFileHandler
 import os
 import time
 
-import requests
 from dotenv import load_dotenv
 from telebot import TeleBot, apihelper
+import requests
 
 import exceptions as EX
 
 
 load_dotenv()
 
-logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(
-    filename='main.log',
-    filemode='w',
-    format='%(asctime)s, %(levelname)s, %(message)s'
-)
 
+logger = logging.getLogger(__name__)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -48,7 +43,7 @@ def check_tokens():
         result = []
         if not value:
             result += [key]
-            logging.critical(
+            logger.critical(
                 'Отсутствует обязательная переменная окружения: '
                 f'{key}. Программа принудительно остановлена.'
             )
@@ -62,26 +57,29 @@ def check_tokens():
 def send_message(bot, message):
     """Отправляет сообщение в Telegram-чат, опр-й переменной окружения."""
     try:
-        logging.debug('Начало отправки сообщения в Telegram')
+        logger.debug('Начало отправки сообщения в Telegram')
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.debug('удачная отправка сообщения в Telegram')
+        logger.debug('удачная отправка сообщения в Telegram')
         return True
     except (apihelper.ApiException, requests.RequestException) as error:
-        logging.error(error, exc_info=True)
+        logger.error(error, exc_info=True)
         return False
 
 
 def get_api_answer(timestamp):
     """Делает запрос к единственному эндпоинту API-сервиса."""
     try:
-        logging.debug('Начало запроса к эндпоинту API-сервиса')
+        logger.debug('Начало запроса к эндпоинту API-сервиса')
         homework_statuses = requests.get(
             ENDPOINT,
             headers=HEADERS,
             params={'from_date': timestamp}
         )
     except requests.RequestException as error:
-        logging.error(f'ошибка при запросе к эндпоинту: {error}')
+        logger.error(
+            'ошибка при запросе c from_date '
+            '"{}" к эндпоинту: {}'.format(timestamp, error)
+        )
         raise EX.ErrorRequestGetApi
 
     if homework_statuses.status_code == HTTPStatus.OK:
@@ -93,13 +91,13 @@ def get_api_answer(timestamp):
 def check_response(response):
     """Проверяет ответ API на соответствие документации из урока."""
     if not isinstance(response, dict):
-        logging.error('response не является dict')
+        logger.error('response не является dict')
         raise EX.ErrorResponseNotDict(f'В response находится {type(response)}')
     elif response.get('homeworks') is None:
-        logging.error('Словарь response не содержит ключ "homeworks"')
+        logger.error('Словарь response не содержит ключ "homeworks"')
         raise EX.ErrorResponseDictKey
     elif not isinstance(response['homeworks'], list):
-        logging.error(
+        logger.error(
             'Значение словаря response содержит ключ "homeworks"'
             ' значением которого не является list'
         )
@@ -112,22 +110,22 @@ def check_response(response):
 def parse_status(homework):
     """Извлекает из инф-ю о конкретной домашней работе статус этой работы."""
     if not homework:
-        logging.debug('отсутствие в ответе новых статусов')
+        logger.debug('отсутствие в ответе новых статусов')
         return None
     elif not isinstance(homework, dict):
-        logging.error('в списке значений "homework" нет ожидаемого словаря')
+        logger.error('в списке значений "homework" нет ожидаемого словаря')
         raise EX.ErrorDictParseStatus
     elif 'status' not in homework:
-        logging.error('в списке значений "homework" нет ожидаемого словаря')
+        logger.error('в списке значений "homework" нет ожидаемого словаря')
         raise EX.ErrorDictKeyParseStatus
     elif homework['status'] not in HOMEWORK_VERDICTS:
-        logging.error(
+        logger.error(
             'неожиданный статус домашней работы, '
             'обнаруженный в ответе API'
         )
         raise EX.ErrorDictKeyStatusInParseStatus
     elif 'homework_name' not in homework:
-        logging.error('в ответе API домашки нет ключа "homework_name"')
+        logger.error('в ответе API домашки нет ключа "homework_name"')
         raise EX.ErrorDictKeyHomeworkNameInParseStatus
     homework_name = homework['homework_name']
     verdict = HOMEWORK_VERDICTS[homework['status']]
@@ -151,7 +149,7 @@ def main():
                     None, response.get('current_date', time.time())
                 ][send_message(bot, message)]
             else:
-                logging.debug('Пустое сообщение не отправлено')
+                logger.debug('Пустое сообщение не отправлено')
         except Exception as error:
             message = f'Возникла ошибка {error}'
             if message_error != message:
@@ -164,6 +162,15 @@ def main():
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    handler = RotatingFileHandler('main.log', maxBytes=50000000, backupCount=5)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s'
+    )
+    handler = RotatingFileHandler(
+        'main.log',
+        maxBytes=50000000,
+        backupCount=5,
+        encoding='utf-8'
+    )
+    handler.setFormatter(formatter)
     logger.addHandler(handler)
     main()
