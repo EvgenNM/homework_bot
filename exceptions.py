18a5a0759afaef4f1db836e3dcec7a37a3e74c56
yevgeny_novik <novik297443991@yandex.ru>
2025-04-15 13:24:28 +0300

    Исправлено все кроме foramat, logger

--- exceptions.py
+++ exceptions.py
@@ -16,3 +16,27 @@ class ErrorResponseNone(Exception):
 
 class ErrorResponseNotDict(TypeError):
     """Исключение ошибки при отсутствии словаря в response"""
+
+
+class ErrorResponseDictKey(ValueError):
+    """Исключение отсутствия ожидаемого ключа homeworks в словаре response."""
+
+
+class ErrorResponseNotList(TypeError):
+    """Исключение отсутствия ожидаемого списка в словаре response """
+
+
+class ErrorDictParseStatus(TypeError):
+    """Исключение отсутствия ожидаемого аргумента - словаря в parse_status"""
+
+
+class ErrorDictKeyParseStatus(ValueError):
+    """Исключение отсутствия ключа в переданном словаре в parse_status"""
+
+
+class ErrorDictKeyStatusInParseStatus(ValueError):
+    """Исключение получения неожиданного статуча довашней работы"""
+
+
+class ErrorDictKeyHomeworkNameInParseStatus(ValueError):
+    """Исключение отсутствия ключа домашки"""
--- homework.py
+++ homework.py
@@ -64,8 +64,10 @@ def send_message(bot, message):
         logging.debug('Начало отправки сообщения в Telegram')
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.debug('удачная отправка сообщения в Telegram')
+        return True
     except (apihelper.ApiException, requests.RequestException) as error:
         logging.error(error, exc_info=True)
+        return False
 
 
 def get_api_answer(timestamp):
@@ -90,25 +92,21 @@ def get_api_answer(timestamp):
 
 def check_response(response):
     """Проверяет ответ API на соответствие документации из урока."""
-    if not response:
-        logging.error('response пуст')
-        raise EX.ErrorResponseNone
-    elif not isinstance(response, dict):
+    if not isinstance(response, dict):
         logging.error('response не является dict')
         raise EX.ErrorResponseNotDict(f'В response находится {type(response)}')
     elif response.get('homeworks') is None:
         logging.error('Словарь response не содержит ключ "homeworks"')
-        raise ValueError
+        raise EX.ErrorResponseDictKey
     elif not isinstance(response['homeworks'], list):
         logging.error(
             'Значение словаря response содержит ключ "homeworks"'
             ' значением которого не является list'
         )
-        raise TypeError
+        raise EX.ErrorResponseNotList
     elif not response['homeworks']:
-        return response['homeworks']
-    else:
-        return response['homeworks'][0]
+        return None
+    return response['homeworks']
 
 
 def parse_status(homework):
@@ -117,41 +115,50 @@ def parse_status(homework):
         logging.debug('отсутствие в ответе новых статусов')
         return None
     elif not isinstance(homework, dict):
-        logging.debug('в списке значений "homework" нет ожидаемого словаря')
-        raise TypeError
-    elif HOMEWORK_VERDICTS.get(homework['status']) is None:
+        logging.error('в списке значений "homework" нет ожидаемого словаря')
+        raise EX.ErrorDictParseStatus
+    elif 'status' not in homework:
+        logging.error('в списке значений "homework" нет ожидаемого словаря')
+        raise EX.ErrorDictKeyParseStatus
+    elif homework['status'] not in HOMEWORK_VERDICTS:
         logging.error(
             'неожиданный статус домашней работы, '
             'обнаруженный в ответе API'
         )
-        raise ValueError
-    elif homework.get('homework_name') is None:
+        raise EX.ErrorDictKeyStatusInParseStatus
+    elif 'homework_name' not in homework:
         logging.error('в ответе API домашки нет ключа "homework_name"')
-        raise ValueError
-    else:
-        homework_name = homework['homework_name']
-        verdict = HOMEWORK_VERDICTS[homework['status']]
-        message = (f'Изменился статус проверки работы '
-                   f'"{homework_name}". {verdict}')
-        return message
+        raise EX.ErrorDictKeyHomeworkNameInParseStatus
+    homework_name = homework['homework_name']
+    verdict = HOMEWORK_VERDICTS[homework['status']]
+    return ('Изменился статус проверки работы '
+            f'"{homework_name}". {verdict}')
 
 
 def main():
     """Основная логика работы бота."""
-    work = False
-    if check_tokens():
-        bot = TeleBot(token=TELEGRAM_TOKEN)
-        timestamp = int(time.time())
-        message_telegram = ''
-        work = True
-    while work:
-        response = get_api_answer(timestamp)
-        homework = check_response(response)
-        message = parse_status(homework)
-        if message and message_telegram != message:
-            send_message(bot, message)
-            message_telegram = message
-        time.sleep(RETRY_PERIOD)
+    check_tokens()
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    timestamp = int(time.time())
+    message_error = ''
+    while True:
+        try:
+            response = get_api_answer(timestamp)
+            homeworks = check_response(response)
+            if homeworks:
+                message = parse_status(homeworks[0])
+                *_, timestamp = [
+                    None, response.get('current_date', time.time())
+                ][send_message(bot, message)]
+            else:
+                logging.debug('Пустое сообщение не отправлено')
+        except Exception as error:
+            message = f'Возникла ошибка {error}'
+            if message_error != message:
+                send_message(bot, message)
+                message_error = message
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
